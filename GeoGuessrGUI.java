356f3fd9caf571618b61775530b9256534b09558
MuhrizMunir <mm7063@pleasantonusd.net>
2025-05-14 22:00:58 +0000

    abhays codes

--- GeoGuessrGUI.java
+++ GeoGuessrGUI.java
@@ -3,8 +3,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.io.File;
-
-
 public class GeoGuessrGUI{
     public static void main(String[] args) {
        SwingUtilities.invokeLater(() -> new GameGUI().startGame());
